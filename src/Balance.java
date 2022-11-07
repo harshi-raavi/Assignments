df83a82cee648226e79ad888fea28bb58eb6a10f
harshi-raavi <116262482+harshi-raavi@users.noreply.github.com>
2022-11-07 09:22:48 +0530

    JUnit Assignment

---
+++ src/Balance.java
@@ -0,0 +1,29 @@
+public class Balance
+{
+    public int withdraw(int amount)
+    {
+        int balance = 5000;
+        if (amount > balance)
+        {
+            throw new NotSufficientFundException(String.format("current balance is less than  required"));
+        }
+
+        return balance;
+    }
+}
+class NotSufficientFundException extends RuntimeException
+{
+    String message;
+    public NotSufficientFundException(String message)
+    {
+
+        this.message=message;
+    }
+
+    public String getMessage()
+    {
+
+
+        return message;
+    }
+}
---
+++ src/MinMaxTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class MinMaxTest {
+    MinMax m1;
+
+    @BeforeEach
+    void init() {
+        m1 = new MinMax();
+    }
+
+    @Test
+    void findMinMax() {
+
+        int[] sample = {31, 7, 1, 8};
+        int[] expected = {1, 31};
+        int[] actual = m1.findMinMax(sample);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findMax() {
+        int[] sample = {31, 7, 1, 8};
+        int expected = 31;
+        int actual = m1.maximum(sample);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        int[] sample = {31, 7, 1, 8};
+        int expected = 1;
+        int actual = m1.minimum(sample);
+        assertEquals(expected, actual);
+    }
+}
