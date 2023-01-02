318f39b5b963acd2bfdc69069eb37bfa86d89f4c
harshi-raavi <116262482+harshi-raavi@users.noreply.github.com>
2023-01-02 10:11:59 +0530

    Add files via upload

---
+++ springBoot/src/main/java/com/springBoot/springBoot/Course.java
@@ -0,0 +1,31 @@
+package com.springBoot.springBoot;
+
+public class Course {
+ private long id;
+ private String name;
+ private String author;
+ //constructor
+public Course(long id, String name, String author) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.author = author;
+}
+//getters
+public long getId() {
+	return id;
+}
+public String getName() {
+	return name;
+}
+public String getAuthor() {
+	return author;
+}
+//toString()
+@Override
+public String toString() {
+	return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+}
+ 
+ 
+}
